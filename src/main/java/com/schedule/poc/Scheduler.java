878ce5f2e250ac24d34b0bd2dbc28df7ea35995b
samckp <sam.mca.liet@gmail.com>
2021-03-19 09:24:54 +0530

    code updated

---
+++ src/main/java/com/schedule/poc/Scheduler.java
@@ -0,0 +1,19 @@
+package com.schedule.poc;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class Scheduler {
+
+    @Scheduled(fixedRate = 10000, initialDelay = 1000)
+    public void cronScheduler(){
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        Date now = new Date();
+        String strDate = sdf.format(now);
+        System.out.println("Java cron job scheduler :: " + strDate);
+    }
+}
